608e5df0f3c39075bcedbe305ce1d171486ae580
Sorn <maksumic@protonmail.com>
2025-04-18 19:16:32 +0200

    Set up Java 21 Maven project

---
+++ core/src/main/java/dev/sorn/matchingengine/OrderBook.java
@@ -0,0 +1,4 @@
+package dev.sorn.matchingengine;
+
+public class OrderBook {
+}
---
+++ core/src/test/java/dev/sorn/matchingengine/OrderBookTest.java
@@ -0,0 +1,10 @@
+package dev.sorn.matchingengine;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderBookTest {
+    @Test
+    public void test() {
+        new OrderBook();
+    }
+}
